3d125d516cb9fd4d81c3e6ce3dde4bbd496c240a
gsush <gsush@LAPTOP-HPJHJJ4F>
2017-05-01 19:24:55 +0530

    mvcDemo3.

---
+++ mvcDemo/src/main/java/com/mvc/controllers/PersonController.java
@@ -0,0 +1,24 @@
+package com.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.mvc.model.Person;
+
+@Controller
+public class PersonController 
+{
+	@RequestMapping("/personForm")
+	public String getPersonForm(Model model)
+	{
+		model.addAttribute("personObj",new Person());
+		return "personform";
+	}
+	@RequestMapping("/processPersonDetails")
+    public String getPersonDetails(@ModelAttribute(value="personObj")  Person person,Model model)
+    {
+    	model.addAttribute("person",person);
+    	return "persondetails";	    	
+	}
+}
